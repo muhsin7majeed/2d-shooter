d3d2f2062effc185ea5e2382bf752c2b3e02ecb0
muhsin7majeed <muhsin7ak@gmail.com>
2025-06-24 14:53:29 +0000

    new: update loader

--- src/components/GameLoading.tsx
+++ src/components/GameLoading.tsx
@@ -1,5 +1,13 @@
 const GameLoading = () => {
-  return <div>GameLoading</div>;
+  return (
+    <div className="game-loading">
+      <div className="game-loading-logo">
+        <img src="./assets/player_jets/player_jet_1.png" alt="logo" />
+      </div>
+
+      <div className="game-loading-text">Loading..</div>
+    </div>
+  );
 };
 
 export default GameLoading;
