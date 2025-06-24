61c5546f9837cab03bee079d30dbcf24c4360ff6
muhsin7majeed <muhsin7ak@gmail.com>
2025-06-24 15:41:50 +0000

    fix: css import

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import { createRoot } from 'react-dom/client';
 import './styles/main.css';
 import 'nes.css/css/nes.min.css';
+import '../node_modules/nes.css/css/nes.css';
 
 import App from './App.tsx';
 
